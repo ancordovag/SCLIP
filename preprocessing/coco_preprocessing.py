9ee95908c8201b3e367e0d8c0bbf084869560e72
Marco Cipriano <marco.cipriano@hpi.de>
2022-08-17 14:52:09 +0200

    Added scripts to extract splits from corpus

--- preprocessing/coco_preprocessing.py
+++ preprocessing/coco_preprocessing.py
@@ -1,15 +1,20 @@
 import json
 import yaml
+import os
 
-def write_coco_files(max_train, max_valid, max_test):
-    directory = 'coco/'
-    file_train = directory + 'captions_train2017.json'
-    file_val = directory + 'captions_val2017.json'
+
+def write_coco_files(max_train, max_valid, max_test, data_dir, out_dir):
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    file_train = os.path.join(data_dir, 'captions_train2017.json')
+    file_val = os.path.join(data_dir, 'captions_val2017.json')
     
-    f_train = open(file_train) # 591753 lines
+    f_train = open(file_train)  # 591753 lines
     train_data = json.load(f_train)
-    train_file = open(directory + 'train_sentences.txt',mode='w',encoding='utf-8')
-    test_file = open(directory + 'test_sentences.txt',mode='w',encoding='utf-8')
+    train_file = open(os.path.join(out_dir, 'train_sentences.txt'), mode='w', encoding='utf-8')
+    test_file = open(os.path.join(out_dir, 'test_sentences.txt'), mode='w', encoding='utf-8')
     count = 0
     train_count = 0
     for element in train_data["annotations"]:
@@ -34,7 +39,7 @@ def write_coco_files(max_train, max_valid, max_test):
     
     f_val = open(file_val)
     val_data = json.load(f_val) # 25014 lines
-    valid_file = open(directory + 'valid_sentences.txt',mode='w',encoding='utf-8')
+    valid_file = open(os.path.join(out_dir, 'valid_sentences.txt'), mode='w', encoding='utf-8')
     # Creating Validation file
     count = 0
     for element in val_data["annotations"]:
@@ -56,8 +61,8 @@ def write_coco_files(max_train, max_valid, max_test):
 if __name__ == "__main__":
     with open("config.yml", "r") as ymlfile:
         cfg = yaml.safe_load(ymlfile)
-    params = cfg["preprocessing"]
+    params = cfg["coco_preprocessing"]
     train_lines = params["max_train_lines"]    
     valid_lines = params["max_valid_lines"]
     test_lines = params["max_test_lines"]
-    write_coco_files(train_lines,valid_lines,test_lines)
+    write_coco_files(train_lines, valid_lines, test_lines, params["data_dir"], params["out_dir"])
--- preprocessing/europarl_preprocessing.py
+++ preprocessing/europarl_preprocessing.py
@@ -1,14 +1,18 @@
 import yaml
 import os
 
-def write_new_files(max_train_lines, max_valid_lines, max_test_lines, dirname='europarl', max_sentence_length=30):
+
+def write_new_files(max_train_lines, max_valid_lines, max_test_lines, data_dir, out_dir, max_sentence_length=30):
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
     # Open the files
-    train_file = open(os.path.join(dirname,'train_sentences.txt'),mode='w',encoding='utf-8')
-    valid_file = open(os.path.join(dirname,'valid_sentences.txt'),mode='w',encoding='utf-8')
-    test_file = open(os.path.join(dirname,'test_sentences.txt'),mode='w',encoding='utf-8')
-    
-    english_dir = 'Europarl.en-es.en'
-    english_filename = os.path.join(dirname,english_dir)
+    train_file = open(os.path.join(out_dir, 'train_sentences.txt'), mode='w', encoding='utf-8')
+    valid_file = open(os.path.join(out_dir, 'valid_sentences.txt'), mode='w', encoding='utf-8')
+    test_file = open(os.path.join(out_dir, 'test_sentences.txt'), mode='w', encoding='utf-8')
+
+    english_filename = os.path.join(data_dir, 'Europarl.en-es.en')
 
     lines_counter = 0
     with open(english_filename, mode='rt', encoding='utf-8') as english_file:
@@ -31,15 +35,15 @@ def write_new_files(max_train_lines, max_valid_lines, max_test_lines, dirname='e
     train_file.close()
     test_file.close()
     valid_file.close()
-    
+
+
 if __name__ == "__main__":
     with open("config.yml", "r") as ymlfile:
         cfg = yaml.safe_load(ymlfile)
 
     params = cfg["preprocessing"]
-    dirname = 'europarl/'    
     train_lines = params["max_train_lines"]
     valid_lines = params["max_valid_lines"]
     test_lines = params["max_test_lines"]
     sentence_length = params["max_sentence_length"]
-    write_new_files(train_lines,valid_lines, test_lines, dirname, sentence_length)
+    write_new_files(train_lines, valid_lines, test_lines, params["data_dir"], params["out_dir"], sentence_length)
